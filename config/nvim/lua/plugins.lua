731d0417b2dde755960b713cadcea61e97173ab2
danilivanyuk <danilivanyuk@gmail.com>
2024-08-31 17:09:54 +0300

    zshrc comments remove nvim lazygit init

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -84,6 +84,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
+        { '<leader>l', group = '[L]azy[G]it' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       }
     end,
@@ -419,24 +420,24 @@ require('lazy').setup({
     keys = {
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
-    {
-      'ggandor/leap.nvim',
-      enabled = true,
-      keys = {
-        { 's', mode = { 'n', 'x', 'o' }, desc = 'Leap Forward to' },
-        { 'S', mode = { 'n', 'x', 'o' }, desc = 'Leap Backward to' },
-        { 'gs', mode = { 'n', 'x', 'o' }, desc = 'Leap from Windows' },
-      },
-      config = function(_, opts)
-        local leap = require 'leap'
-        for k, v in pairs(opts) do
-          leap.opts[k] = v
-        end
-        leap.add_default_mappings(true)
-        vim.keymap.del({ 'x', 'o' }, 'x')
-        vim.keymap.del({ 'x', 'o' }, 'X')
-      end,
+  },
+  {
+    'ggandor/leap.nvim',
+    enabled = true,
+    keys = {
+      { 's', mode = { 'n', 'x', 'o' }, desc = 'Leap Forward to' },
+      { 'S', mode = { 'n', 'x', 'o' }, desc = 'Leap Backward to' },
+      { 'gs', mode = { 'n', 'x', 'o' }, desc = 'Leap from Windows' },
     },
+    config = function(_, opts)
+      local leap = require 'leap'
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      vim.keymap.del({ 'x', 'o' }, 'x')
+      vim.keymap.del({ 'x', 'o' }, 'X')
+    end,
   },
 }, {
   ui = {
